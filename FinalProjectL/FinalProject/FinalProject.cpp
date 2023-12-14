e3d7c7ff153e36b0f8a6a25a1d55671ed9454cb6
n0P7xJ <124531921+n0P7xJ@users.noreply.github.com>
2023-12-14 20:19:00 +0200

    Update SFML 2_61

--- FinalProjectL/FinalProject/FinalProject.cpp
+++ FinalProjectL/FinalProject/FinalProject.cpp
@@ -1,14 +1,28 @@
-﻿#include <iostream>
-#include "SFML/Graphics.hpp"
+﻿#include "SFML/Graphics.hpp"
+#include <iostream>
+#include <exception>
 
 
-int main()
-{
-    //task
+int main(){
+    try {
+        sf::RenderWindow mainWindow;
+        mainWindow.create(sf::VideoMode(1024, 640), L"DailyTask");
 
-    //це створює змінну n для чогось
-    int n = NULL;
+        while (mainWindow.isOpen()) {
+            sf::Event event;
+            while (mainWindow.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    mainWindow.close();
+                }
+            }
 
-    system("pause");
-    return 0;
+
+
+        }
+        system("pause");
+        return 0;
+    }
+    catch (...) {
+        std::cerr << "Code have problem" << '\n';
+    }
 }
