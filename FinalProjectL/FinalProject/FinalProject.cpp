2fff2945736f8ae27b93cf90093b24ed2df06ea5
n0P7xJ <124531921+n0P7xJ@users.noreply.github.com>
2024-01-06 17:58:01 +0200

    Fix bug, пофіксив баг з запуском на різник компах

--- FinalProjectL/FinalProject/FinalProject.cpp
+++ FinalProjectL/FinalProject/FinalProject.cpp
@@ -1,24 +1,46 @@
 ﻿#include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+int main(){
+    sf::RenderWindow window(sf::VideoMode(1170, 768), "DailyTask");
+
+    // Створюю двухвимірний масив кнопок, для подальшого виводу на екран
+    const int LineHorizont = 7;
+    const int LineVertikal = 7;
+    sf::RectangleShape listRectangl[LineHorizont][LineVertikal]; // Статичний оскільки масив не буде міняти свої розміри
+    // sf::RectangleShape (*ptrListRectangle)[LineVertikal] = listRectangl; Вказівник на масив якщо буде потрібен нараз в коментарі
+    // Структура вказівника (вказівник на масив)[розмір масива], не створив динамічний оскільки розмір не буде мінятися
+    
+    // Тимчасовий варіант заповнення
+
+    sf::RectangleShape rectangleYearAndMouth;
+    // sf::RectangleShape* rectangleYearAndMourh;
+    rectangleYearAndMouth.setSize(sf::Vector2f(1170, 96));
+    rectangleYearAndMouth.setPosition(sf::Vector2f(0, 0));
+    rectangleYearAndMouth.setFillColor(sf::Color::Black);
+
+    for (int i = 0; i < LineHorizont; ++i) {
+        for (int j = 0; j < LineVertikal; ++j) {
+            sf::RectangleShape rectangl;
+            
+
+            //listRectangl[i][j]
+        }
+    }
+
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::White);
+        window.draw(rectangleYearAndMouth);
         window.display();
     }
+    
 
     return 0;
 }
---
+++ FinalProjectL/FinalProject/TextBox.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+class textBox : public sf::RectangleShape
+{
+
+
+
+};
