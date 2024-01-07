21de8675cb70ccdea227f41fc20709820a2eaaf3
n0P7xJ <124531921+n0P7xJ@users.noreply.github.com>
2024-01-07 11:23:42 +0200

    Зміни

--- FinalProjectL/FinalProject/UsersClass/SystemTime.cpp
+++ FinalProjectL/FinalProject/UsersClass/SystemTime.cpp
@@ -1,16 +1,20 @@
 #include "SystemTime.h"
 
-void SystemTime::Output()
-{
-	while (true)
-	{
-		GetLocalTime(&sTIME);
-
-		std::cout << "\t" << sTIME.wHour << ":" << sTIME.wMinute << ":" << sTIME.wSecond
-			<< std::endl << "\t" << sTIME.wDay << "." << sTIME.wMonth << "." << sTIME.wYear;
-
-		Sleep(1000);
-
-		system("cls");
-	}
+int SystemTime::getHour() const{
+	 return SYSTEMTIME::wHour;
 }
+
+//void SystemTime::Output()
+//{
+//	while (true)
+//	{
+//		GetLocalTime(&sTIME);
+//
+//		std::cout << "\t" << sTIME.wHour << ":" << sTIME.wMinute << ":" << sTIME.wSecond
+//			<< std::endl << "\t" << sTIME.wDay << "." << sTIME.wMonth << "." << sTIME.wYear;
+//
+//		Sleep(1000);
+//
+//		system("cls");
+//	}
+//}
--- FinalProjectL/FinalProject/UsersClass/SystemTime.h
+++ FinalProjectL/FinalProject/UsersClass/SystemTime.h
@@ -3,22 +3,15 @@
 #include <iostream>
 #include <Windows.h>
 
-class SystemTime
-{
-	int hour, minute, second;
-	int day, month, year;
-	SYSTEMTIME sTIME;
+class SystemTime : protected SYSTEMTIME{
 public:
-	int GetHour() const { return hour; }
-	int GetMinute() const { return minute; }
+	int getHour() const;
+	/*int GetMinute() const { return minute; }
 	int GetSecond() const { return second; }
 
 	int GetDay() const { return day; }
 	int GetMonth() const { return month; }
-	int GetYear() const { return year; }
-
-	SystemTime() : hour(sTIME.wHour), minute(sTIME.wMinute), second(sTIME.wSecond),
-		day(sTIME.wDay), month(sTIME.wMonth), year(sTIME.wYear) {}
+	int GetYear() const { return year; }*/
 
 	void Output();
 };
