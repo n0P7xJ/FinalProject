41abdbd71fc05672c0f3b84e8b26a2b6a276ba21
n0P7xJ <124531921+n0P7xJ@users.noreply.github.com>
2024-01-18 21:29:35 +0200

    Fix bug, modification/Фіксанув баги, модифікував код

---
+++ FinalProjectL/FinalProject/TextBox.cpp
@@ -0,0 +1,50 @@
+#include "TextBox.h"
+
+//Text Default Value
+ const unsigned int TextBox::defaultCharacterSize = 36;
+ const sf::Color TextBox::defaultFillColorText = sf::Color::Black;
+ const sf::Font TextBox::defaultFontText = [] {
+    sf::Font font;
+    if (!font.loadFromFile("Font/arial.ttf")) {
+        throw std::runtime_error("Failed to load font");
+    }
+    return font;
+}();
+ const std::string TextBox::defaultString = "empty";
+//Rectangle Default Value
+ const sf::Color TextBox::defaultFillColorBox = sf::Color::White;
+ const sf::Vector2f TextBox::defaultSize(1.f,1.f);
+//Other 
+ const sf::Vector2f defaultPosition(0.f, 0.f);
+
+TextBox::TextBox(const sf::Vector2f& size, const sf::Vector2f& position, const std::string& content, const sf::Color& fillColorBox, const sf::Font& font, const sf::Color& fillColor, unsigned int characterSize) {
+    setRectangleProperties(size, position, fillColorBox);
+    setTextProperties(content, font, fillColor, characterSize);
+}
+
+void TextBox::setRectangleProperties(const sf::Vector2f& size, const sf::Vector2f& position, const sf::Color fillColor) {
+    sf::RectangleShape::setSize(size);
+    sf::RectangleShape::setPosition(position);
+    sf::RectangleShape::setFillColor(fillColor);
+    // Настройка коліру вздовш ліній прямокутника
+    /*sf::RectangleShape::setOutlineColor(sf::Color::Black);
+    sf::RectangleShape::setOutlineThickness(2.0f);*/
+}
+
+void TextBox::setTextProperties(const std::string& content, const sf::Font& font, const sf::Color& fillColor,const unsigned int& characterSize) {
+    sf::Text::setString(content);
+    sf::Text::setFont(font);
+    sf::Text::setFillColor(fillColor);
+    sf::Text::setCharacterSize(characterSize);
+
+    // Центруємо текст у прямокутнику
+    sf::FloatRect textBounds = sf::RectangleShape::getLocalBounds();
+    sf::RectangleShape::setOrigin(textBounds.left + textBounds.width / 2.0f, textBounds.top + textBounds.height / 2.0f);
+    sf::RectangleShape::setPosition(sf::RectangleShape::getPosition().x + sf::RectangleShape::getSize().x / 2.0f, sf::RectangleShape::getPosition().y + sf::RectangleShape::getSize().y / 2.0f);
+}
+
+void TextBox::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+    // Тут ви маєте викликати методи малювання вашого об'єкта
+    target.draw(static_cast<sf::RectangleShape>(*this), states);
+    target.draw(static_cast<sf::Text>(*this), states);
+}
---
+++ FinalProjectL/FinalProject/TextBox.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <exception>
+
+class TextBox : public sf::RectangleShape, public sf::Text, public sf::Drawable {
+public:
+    TextBox(const sf::Vector2f& size, const sf::Vector2f& position, const std::string& content = defaultString, const sf::Color& fillColorBox = defaultFillColorBox, const sf::Font& font = defaultFontText, const sf::Color& fillColor = defaultFillColorText, unsigned int characterSize = defaultCharacterSize);
+
+    // Метод для задання властивостей прямокутника
+    void setRectangleProperties(const sf::Vector2f& size, const sf::Vector2f& position, const sf::Color fillColor = defaultFillColorBox);
+
+    // Метод для задання властивостей тексту
+    void setTextProperties(const std::string& content, const sf::Font& font, const sf::Color& fillColor,const unsigned int& characterSize);
+
+    //Draw
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states = sf::RenderStates::Default) const;
+private:
+    // Text default value
+    static const unsigned int defaultCharacterSize;
+    static const sf::Color defaultFillColorText;
+    static const sf::Font defaultFontText;
+    static const std::string defaultString;
+    // Rectangle default value
+    static const sf::Color defaultFillColorBox;
+    static const sf::Vector2f defaultSize;
+    // Other
+    static const sf::Vector2f defaultPosition;
+};
